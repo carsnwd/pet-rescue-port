0a25dbe899982a5c02949ffdd77933ff2881c6d1
carson <carsnwd@gmail.com>
2021-12-11 19:48:00 -0500

    ✨ add in react-router

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,11 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
+import Home from './layouts/Home'
+import Settings from './layouts/Settings'
 
 function App() {
 
   return (
     <div className="flex">
       <Sidebar/>
-      <div className="flex-col bg-slate-100 w-screen">Content</div>
+      <div className="flex-col bg-slate-100 w-screen">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="settings" element={<Settings/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
--- apps/admin/src/components/Sidebar.tsx
+++ apps/admin/src/components/Sidebar.tsx
@@ -1,3 +1,8 @@
+import { Link } from "react-router-dom";
+
 export default function Sidebar() {
-    return <div className="bg-indigo-900 w-80 flex-col h-screen">Sidebar</div>;
+    return <div className="bg-indigo-900 w-80 flex-col h-screen">
+        <Link to="/">Home</Link>
+        <Link to="/settings">Settings</Link>
+    </div>;
 }
