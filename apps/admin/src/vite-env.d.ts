15e3695a01f2f8dacd63fb5793ed13cde43d7d71
carsnwd <carsnwd@gmail.com>
2021-12-21 13:01:29 -0500

    Setup .env file config for firebase

--- apps/admin/src/vite-env.d.ts
+++ apps/admin/src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_FIREBASE: {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+    measurementId: string;
+  };
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
