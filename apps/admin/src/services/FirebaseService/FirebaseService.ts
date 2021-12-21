46432761218fd564d66298d69d9f646ee7c29eee
carsnwd <carsnwd@gmail.com>
2021-12-21 13:02:00 -0500

    init firebase service

---
+++ apps/admin/src/services/FirebaseService/FirebaseService.ts
@@ -0,0 +1,32 @@
+import { FirebaseApp, initializeApp } from "firebase/app";
+import.meta.env.VITE_FIREBASE;
+
+class FirebaseService {
+  private static instance: FirebaseService;
+
+  private firebase: FirebaseApp;
+
+  private constructor() {
+    this.firebase = this.initializeFirebaseApp();
+  }
+
+  private initializeFirebaseApp(): FirebaseApp {
+    const firebaseConfig = {
+      apiKey: import.meta.env.VITE_FIREBASE.apiKey,
+      authDomain: import.meta.env.VITE_FIREBASE.authDomain,
+      projectId: import.meta.env.VITE_FIREBASE.projectId,
+      storageBucket: import.meta.env.VITE_FIREBASE.storageBucket,
+      messagingSenderId: import.meta.env.VITE_FIREBASE.messagingSenderId,
+      appId: import.meta.env.VITE_FIREBASE.appId,
+      measurementId: import.meta.env.VITE_FIREBASE.measurementId,
+    };
+    return initializeApp(firebaseConfig);
+  }
+
+  public static getFirebaseInstance(): FirebaseService {
+    if (!FirebaseService.instance) {
+      FirebaseService.instance = new FirebaseService();
+    }
+    return FirebaseService.instance;
+  }
+}
